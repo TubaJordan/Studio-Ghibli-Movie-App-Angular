508b0052ef769e776ad75a33cec19020c124154b
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-10-01 15:04:24 -0700

    Signup and Login components added

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.css']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() userData = { username: '', password: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+  }
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((data) => {
+      localStorage.setItem("user", JSON.stringify(data.user))
+      localStorage.setItem("token", data.token);
+      localStorage.setItem('Username', data.user.Username)
+      this.dialogRef.close();
+      this.snackBar.open('you\'ve been logged in', 'OK', {
+        duration: 2000
+      });
+    }, () => {
+      this.snackBar.open('sorry, something went wrong. please try again', 'OK', {
+        duration: 2000
+      });
+    })
+  }
+}
