0ed87b2592c86914b45463d225b50cf119a3ffbe
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-10-01 15:07:13 -0700

    commenting & formatting fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-// src/app/app.component.ts
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
